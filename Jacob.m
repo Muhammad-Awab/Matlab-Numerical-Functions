e6f2f63c37972d5c0e1467c5def35ef15dad9f8a
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-10-20 10:39:57 +0500

    Create Jacob.m

---
+++ Jacob.m
@@ -0,0 +1,17 @@
+function[x,k]=jacob_i(a,b,tol,p,maxstep)
+n=length(b);
+x=zeros(n,1)
+
+for k=1:maxstep
+   p=x;
+   for j=1:n
+       x(j)=(b(j)-a(j,1:j-1)*p(1:j-1)-a(j,j+1:n)*p(j+1:n))/a(j,j);
+   end
+   err=abs(p-x)
+   if err<tol
+       break;
+   else
+       continue
+   end
+end
+end
