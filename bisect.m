758d6baebd867c4ade446b73723c26ecb893ff2b
Muhammad Awab <98603853+Muhammad-Awab@users.noreply.github.com>
2023-11-02 22:42:18 +0500

    Update bisect.m

--- bisect.m
+++ bisect.m
@@ -1,30 +1,21 @@
-function [c] = bisect(f,a,b,tol,max_iter)
-fun = input('Please enter function: ','s');
-f = inline(fun);
-a = input('Enter the initial value: ');
-b = input('Enter the second value: ');
-max_iter = input('Enter the iterations: ');
-tol = input('Enter the tolernce value: ');
-if (f(a)*f(b) > 0)
-    fprintf('Worng Guess!Enter new guess\n');
-    a = input("Enter the initial value");
-    b = input("Enter the second value");
+function c = bisect( tol,max_iter )
+fun=input('Enter Function: ','s');
+f= inline(fun);
+a=input('Enter Start Value of interval: ');
+b=input('Enter End Value of interval: ');
+tol=input('Enter Tolerance: ');
+max_iter=input('Enter Max Iterations: ');
+disp('k        c          error')
+for k=1:max_iter    
+c=(a+b)/2;
+    if((f(a)*f(c))<0)
+            b=c;
+    elseif((f(b)*f(c))<0)
+                a=c;
+    end
+    error=abs(b-a);
+    if(error<tol)
+        break;
+    end
+fprintf('%d    %f       %f\n',k,c,error)
 end
-disp('k    a      c       b        f(a)       f(c)      f(b)       abs(b-a)')
-for k = 1:max_iter
-c = ((a + b) / 2);
-fprintf('%i    %f     %f        %f        %f      %f       %f    %f\n',k,a,c,b,f(a),f(c),f(b),abs(b-a))   
-if ((f(b)*f(c)) < 0)
-    a = c;
-else
-if ((f(a)*f(c)) < 0)
-    b = c;
-end
-err = abs(b - a);
-if (err < tol)
-    break;
-end
-fprintf('%d    %f     %f\n',k,c,err)
-end
-end
-
